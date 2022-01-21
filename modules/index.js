2044468daa89aa1f956dadaec9ba1571af939d9d
50l3r <500l3r@gmail.com>
2022-01-21 10:01:16 +0100

    Add export endpoint

--- modules/index.js
+++ modules/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable prefer-promise-reject-errors */
-import Vue from 'vue';
-import sdk from '../config/gestios';
+import Vue from "vue";
+import sdk from "../config/gestios";
 
-import cache from './_cache';
-import utils from './_utils';
-import store from '../store';
+import cache from "./_cache";
+import utils from "./_utils";
+import store from "../store";
 
-export default function ({ project = '', url, events }) {
+export default function ({ project = "", url, events }) {
 	return new Vue({
 		data() {
 			return {
@@ -23,49 +23,51 @@ export default function ({ project = '', url, events }) {
 				// Configurations
 				config: null,
 				// Media files
-				media: null
+				media: null,
 			};
 		},
 		computed: {
 			url: {
 				get() {
-					return this.$store.getters['gestios/url'];
+					return this.$store.getters["gestios/url"];
 				},
 				set(value) {
-					return this.$store.dispatch('gestios/url', value);
-				}
+					return this.$store.dispatch("gestios/url", value);
+				},
 			},
 			api() {
-				return this.$store.getters['gestios/api'];
+				return this.$store.getters["gestios/api"];
 			},
 			connected() {
-				return this.$store.getters['gestios/connected'];
+				return this.$store.getters["gestios/connected"];
 			},
 			profile() {
-				return this.$store.getters['gestios/user'];
+				return this.$store.getters["gestios/user"];
 			},
 			avatars() {
-				return this.$store.getters['gestios/avatars/get'];
-			}
+				return this.$store.getters["gestios/avatars/get"];
+			},
 		},
 		async beforeCreate() {
-			this.$on('message-success', events['message-success']);
-			this.$on('message-error', events['message-error']);
+			this.$on("message-success", events["message-success"]);
+			this.$on("message-error", events["message-error"]);
 
 			this.$cache = cache(this.$store, this);
 
 			Vue.prototype.$gestios = this;
 
-			const storedProject = this.$store.getters['gestios/project'];
+			const storedProject = this.$store.getters["gestios/project"];
 			if (storedProject) project = storedProject;
 
 			sdk.project = project;
 			sdk.url = url;
 
-			this.$store.commit('GESTIOS/URL', url);
-			this.$store.commit('GESTIOS/PROJECT', project);
+			this.$store.commit("GESTIOS/URL", url);
+			this.$store.commit("GESTIOS/PROJECT", project);
 
-			sdk.token = this.$store.getters['gestios/user'] ? this.$store.getters['gestios/user'].API : '';
+			sdk.token = this.$store.getters["gestios/user"]
+				? this.$store.getters["gestios/user"].API
+				: "";
 
 			if (storedProject) this.$utils = await utils(this.$store);
 		},
@@ -73,65 +75,194 @@ export default function ({ project = '', url, events }) {
 			const self = this;
 
 			this.project = {
-				name: self.$store.getters['gestios/project'],
-				set: (value) => self.$store.dispatch('gestios/project', value)
+				name: self.$store.getters["gestios/project"],
+				set: (value) => self.$store.dispatch("gestios/project", value),
 			};
 
 			this.config = {
 				list(keys = []) {
-					return self.$store.dispatch('gestios/config/list', keys);
+					return self.$store.dispatch("gestios/config/list", keys);
 				},
-				edit: (keys) => self.$store.dispatch('gestios/config/edit', keys),
-				del: (keys) => self.$store.dispatch('gestios/config/delete', keys)
+				edit: (keys) =>
+					self.$store.dispatch("gestios/config/edit", keys),
+				del: (keys) =>
+					self.$store.dispatch("gestios/config/delete", keys),
 			};
 
 			// User auth
 			this.auth = {
-				login: ({ username, password, project }) => self.$store.dispatch('gestios/auth/login', { username, password, project }),
-				forgot: (email) => self.$store.dispatch('gestios/auth/forgot', email),
-				restore: ({ email, authcode, password }) => self.$store.dispatch('gestios/auth/restore', { email, authcode, password }),
-				logout: () => self.$store.dispatch('gestios/auth/logout'),
-				roles: (id = null) => self.$store.dispatch('gestios/roles/list', id),
-				check: ({ user = null, scopes = [], strict = true }) => self.$store.getters['gestios/roles/check']({ user, scopes, strict }),
-				section: (item) => self.$store.getters['gestios/roles/section'](item)
+				login: ({ username, password, project }) =>
+					self.$store.dispatch("gestios/auth/login", {
+						username,
+						password,
+						project,
+					}),
+				forgot: (email) =>
+					self.$store.dispatch("gestios/auth/forgot", email),
+				restore: ({ email, authcode, password }) =>
+					self.$store.dispatch("gestios/auth/restore", {
+						email,
+						authcode,
+						password,
+					}),
+				logout: () => self.$store.dispatch("gestios/auth/logout"),
+				roles: (id = null) =>
+					self.$store.dispatch("gestios/roles/list", id),
+				check: ({ user = null, scopes = [], strict = true }) =>
+					self.$store.getters["gestios/roles/check"]({
+						user,
+						scopes,
+						strict,
+					}),
+				section: (item) =>
+					self.$store.getters["gestios/roles/section"](item),
 			};
 
 			// Users
 			this.users = {
-				list: ({ page = 1, search = null, limit = 10, reset = false, silent = false } = {}) => self.$store.dispatch('gestios/users/list', { page, search, limit, reset, silent }),
-				get: (id) => self.$store.dispatch('gestios/users/get', id),
-				add: ({ email, nick, name, phone = null, group = null, password = null }) => self.$store.dispatch('gestios/users/add', { email, nick, name, phone, group, password }),
-				edit: ({ id, email, nick, name, phone = null, group = null, password = null }) => self.$store.dispatch('gestios/users/edit', { id, email, nick, name, phone, group, password }),
-				status: (id) => self.$store.dispatch('gestios/users/status', id),
-				delete: (id) => self.$store.dispatch('gestios/users/delete', id)
+				list: ({
+					page = 1,
+					search = null,
+					limit = 10,
+					reset = false,
+					silent = false,
+				} = {}) =>
+					self.$store.dispatch("gestios/users/list", {
+						page,
+						search,
+						limit,
+						reset,
+						silent,
+					}),
+				get: (id) => self.$store.dispatch("gestios/users/get", id),
+				add: ({
+					email,
+					nick,
+					name,
+					phone = null,
+					group = null,
+					password = null,
+				}) =>
+					self.$store.dispatch("gestios/users/add", {
+						email,
+						nick,
+						name,
+						phone,
+						group,
+						password,
+					}),
+				edit: ({
+					id,
+					email,
+					nick,
+					name,
+					phone = null,
+					group = null,
+					password = null,
+				}) =>
+					self.$store.dispatch("gestios/users/edit", {
+						id,
+						email,
+						nick,
+						name,
+						phone,
+						group,
+						password,
+					}),
+				status: (id) =>
+					self.$store.dispatch("gestios/users/status", id),
+				delete: (id) =>
+					self.$store.dispatch("gestios/users/delete", id),
 			};
 
 			// Groups
 			this.groups = {
-				list: ({ reset = false, silent = false } = {}) => self.$store.dispatch('gestios/groups/list', { reset, silent })
+				list: ({ reset = false, silent = false } = {}) =>
+					self.$store.dispatch("gestios/groups/list", {
+						reset,
+						silent,
+					}),
 			};
 
 			// Emails
 			this.emails = {
-				list: ({ page = 1, folder = null, reset = false, silent = false } = {}) => self.$store.dispatch('gestios/emails/list', { page, folder, reset, silent }),
-				folders: ({ page = 1, reset = true, silent = false }) => self.$store.dispatch('gestios/emails/folders', { page, reset, silent }),
-				send: ({ email, subject, message, cc = [], replyto = '', folder = '' }) => self.$store.dispatch('gestios/emails/send', { email, subject, message, cc, replyto, folder }),
-				delete: (id) => self.$store.dispatch('gestios/emails/delete', id)
+				list: ({
+					page = 1,
+					folder = null,
+					reset = false,
+					silent = false,
+				} = {}) =>
+					self.$store.dispatch("gestios/emails/list", {
+						page,
+						folder,
+						reset,
+						silent,
+					}),
+				folders: ({ page = 1, reset = true, silent = false }) =>
+					self.$store.dispatch("gestios/emails/folders", {
+						page,
+						reset,
+						silent,
+					}),
+				send: ({
+					email,
+					subject,
+					message,
+					cc = [],
+					replyto = "",
+					folder = "",
+				}) =>
+					self.$store.dispatch("gestios/emails/send", {
+						email,
+						subject,
+						message,
+						cc,
+						replyto,
+						folder,
+					}),
+				delete: (id) =>
+					self.$store.dispatch("gestios/emails/delete", id),
 			};
 
 			// Media files
 			this.media = {
-				list: ({ page = 1, limit = 20, order = null, folder = null, type = null, search = null, reset = false, silent = false } = {}) => self.$store.dispatch('gestios/media/list', { page, limit, order, folder, type, search, reset, silent }),
-				get: (id) => self.$store.dispatch('gestios/media/get', id),
-				share: ({ id, ts }) => self.$store.dispatch('gestios/media/share', { id, ts }),
-				add: ({ file, name, folder = '' }) => self.$store.dispatch('gestios/media/add', { file, name, folder }),
-				delete: (id) => self.$store.dispatch('gestios/media/delete', id)
+				list: ({
+					page = 1,
+					limit = 20,
+					order = null,
+					folder = null,
+					type = null,
+					search = null,
+					reset = false,
+					silent = false,
+				} = {}) =>
+					self.$store.dispatch("gestios/media/list", {
+						page,
+						limit,
+						order,
+						folder,
+						type,
+						search,
+						reset,
+						silent,
+					}),
+				get: (id) => self.$store.dispatch("gestios/media/get", id),
+				share: ({ id, ts }) =>
+					self.$store.dispatch("gestios/media/share", { id, ts }),
+				add: ({ file, name, folder = "" }) =>
+					self.$store.dispatch("gestios/media/add", {
+						file,
+						name,
+						folder,
+					}),
+				delete: (id) =>
+					self.$store.dispatch("gestios/media/delete", id),
 			};
 		},
 		methods: {
 			// List apps
 			async apps() {
-				return this.$store.dispatch('gestios/apps/list');
+				return this.$store.dispatch("gestios/apps/list");
 			},
 			// Get app
 			app(appName) {
@@ -139,34 +270,107 @@ export default function ({ project = '', url, events }) {
 
 				return {
 					data() {
-						return self.$store.dispatch('gestios/apps/list');
+						return self.$store.dispatch("gestios/apps/list");
 					},
 					field(fieldName) {
 						return new Promise((resolve, reject) => {
-							self.$store.dispatch('gestios/apps/list').then(() => {
-								const field = self.$store.getters['gestios/apps/field'](appName, fieldName);
-								if (field) resolve(field);
-								reject(false);
-							}).catch(() => {
-								reject(false);
-							});
+							self.$store
+								.dispatch("gestios/apps/list")
+								.then(() => {
+									const field = self.$store.getters[
+										"gestios/apps/field"
+									](appName, fieldName);
+									if (field) resolve(field);
+									reject(false);
+								})
+								.catch(() => {
+									reject(false);
+								});
 						});
-					}
+					},
 				};
 			},
 			// Authentication
 			items(app) {
 				return {
-					list: ({ page = 1, filters = null, order = null, limit = 20, silent = false, reset = false, view = null } = {}) => this.$store.dispatch('gestios/items/list', { app, page, filters, order, limit, silent, reset, view }),
-					get: (id, silent = false) => this.$store.dispatch('gestios/items/get', { app, id, silent }),
-					add: ({ params, view = null, callback = null }) => this.$store.dispatch('gestios/items/add', { app, params, view, callback }),
-					edit: ({ id, params, callback = null }) => this.$store.dispatch('gestios/items/edit', { app, id, params, callback }),
-					status: (id) => this.$store.dispatch('gestios/items/status', { app, id }),
-					delete: ({ id, callback = null }) => this.$store.dispatch('gestios/items/delete', { app, id, callback }),
-					auth: ({ scopes = [], item, strict = true, user = null }) => this.$store.getters['gestios/items/auth']({ app, scopes, item, strict, user })
+					list: ({
+						page = 1,
+						filters = null,
+						order = null,
+						limit = 20,
+						silent = false,
+						reset = false,
+						view = null,
+					} = {}) =>
+						this.$store.dispatch("gestios/items/list", {
+							app,
+							page,
+							filters,
+							order,
+							limit,
+							silent,
+							reset,
+							view,
+						}),
+					get: (id, silent = false) =>
+						this.$store.dispatch("gestios/items/get", {
+							app,
+							id,
+							silent,
+						}),
+					add: ({ params, view = null, callback = null }) =>
+						this.$store.dispatch("gestios/items/add", {
+							app,
+							params,
+							view,
+							callback,
+						}),
+					edit: ({ id, params, callback = null }) =>
+						this.$store.dispatch("gestios/items/edit", {
+							app,
+							id,
+							params,
+							callback,
+						}),
+					status: (id) =>
+						this.$store.dispatch("gestios/items/status", {
+							app,
+							id,
+						}),
+					delete: ({ id, callback = null }) =>
+						this.$store.dispatch("gestios/items/delete", {
+							app,
+							id,
+							callback,
+						}),
+					auth: ({ scopes = [], item, strict = true, user = null }) =>
+						this.$store.getters["gestios/items/auth"]({
+							app,
+							scopes,
+							item,
+							strict,
+							user,
+						}),
+					export: ({
+						page = 1,
+						filters = null,
+						order = null,
+						limit = 20,
+						silent = false,
+						fields,
+					} = {}) =>
+						this.$store.dispatch("gestios/items/export", {
+							app,
+							page,
+							filters,
+							order,
+							limit,
+							silent,
+							fields,
+						}),
 				};
-			}
+			},
 		},
-		store
+		store,
 	});
 }
--- store/modules/items.js
+++ store/modules/items.js
@@ -1,6 +1,6 @@
-import Vue from 'vue';
-import gestios from '../../config/gestios';
-import utils from '../../config/utils';
+import Vue from "vue";
+import gestios from "../../config/gestios";
+import utils from "../../config/utils";
 
 const state = {
 	vault: [
@@ -10,45 +10,49 @@ const state = {
 		// 	total: 0,
 		// 	results: []
 		// }
-	]
+	],
 };
 
 const getters = {
 	// Get all items
-	'gestios/items/list': (st) => (view) => {
+	"gestios/items/list": (st) => (view) => {
 		const index = st.vault.findIndex((item) => item.view === view);
 		if (index > -1) return st.vault[index];
 
 		return false;
 	},
 	// Get specific item (Find in all apps)
-	'gestios/items/get': (st) => (app, id) => {
+	"gestios/items/get": (st) => (app, id) => {
 		for (let i = 0; i < st.vault.length; i += 1) {
 			const view = st.vault[i];
 
 			if (view.app === app) {
-				const index = view.results.findIndex((item) => item._EntityId === id);
+				const index = view.results.findIndex(
+					(item) => item._EntityId === id
+				);
 				if (index > -1) return view.results[index];
 			}
 		}
 
 		return false;
 	},
-	'gestios/items/auth': (st, gt) => ({ app, scopes = [], item, strict = true, user = null }) => {
-		try {
-			if (!user) user = gt['gestios/user'];
-			return gestios.app(app).auth({ scopes, item, strict, user });
-		} catch (err) {
-			console.error('No se pudo obtener las aplicaciones', err);
-		}
+	"gestios/items/auth":
+		(st, gt) =>
+		({ app, scopes = [], item, strict = true, user = null }) => {
+			try {
+				if (!user) user = gt["gestios/user"];
+				return gestios.app(app).auth({ scopes, item, strict, user });
+			} catch (err) {
+				console.error("No se pudo obtener las aplicaciones", err);
+			}
 
-		return false;
-	}
+			return false;
+		},
 };
 
 const mutations = {
 	// Wipe view data
-	'GESTIOS/ITEMS/WIPE': function (st, view) {
+	"GESTIOS/ITEMS/WIPE": function (st, view) {
 		const index = st.vault.findIndex((item) => item.view === view);
 		if (index > -1) {
 			st.vault[index].total = 0;
@@ -56,7 +60,10 @@ const mutations = {
 		}
 	},
 	// Add items to cache
-	'GESTIOS/ITEMS/LIST': function (st, { view, app, data, total = 0, reset = false }) {
+	"GESTIOS/ITEMS/LIST": function (
+		st,
+		{ view, app, data, total = 0, reset = false }
+	) {
 		const index = st.vault.findIndex((item) => item.view === view);
 
 		if (index === -1) {
@@ -64,24 +71,30 @@ const mutations = {
 				app,
 				view,
 				results: data,
-				total
+				total,
 			});
 		} else if (reset) {
-			Vue.set(st.vault[index], 'results', data);
-			Vue.set(st.vault[index], 'total', total);
+			Vue.set(st.vault[index], "results", data);
+			Vue.set(st.vault[index], "total", total);
 		} else {
-			Vue.set(st.vault[index], 'total', total);
-			Vue.set(st.vault[index], 'results', st.vault[index].results.concat(data));
+			Vue.set(st.vault[index], "total", total);
+			Vue.set(
+				st.vault[index],
+				"results",
+				st.vault[index].results.concat(data)
+			);
 		}
 	},
 	// Update item if exists
-	'GESTIOS/ITEMS/EDIT': function (st, { app, data, ignore = null }) {
+	"GESTIOS/ITEMS/EDIT": function (st, { app, data, ignore = null }) {
 		if (!Array.isArray(data)) data = [data];
 
 		st.vault.forEach((view) => {
 			if (view.app === app && ignore !== app) {
 				data.forEach((d) => {
-					const index = view.results.findIndex((item) => item._EntityId === d._EntityId);
+					const index = view.results.findIndex(
+						(item) => item._EntityId === d._EntityId
+					);
 					if (index > -1) {
 						const item = view.results[index];
 						const mixedItem = { ...item, ...d };
@@ -93,49 +106,81 @@ const mutations = {
 		});
 	},
 	// Add item
-	'GESTIOS/ITEMS/ADD': function (st, { view, data }) {
+	"GESTIOS/ITEMS/ADD": function (st, { view, data }) {
 		const index = st.vault.findIndex((item) => item.view === view);
 		if (index > -1) {
 			st.vault[index].results.unshift(data);
-			Vue.set(st.vault[index], 'total', st.vault[index].total + 1);
+			Vue.set(st.vault[index], "total", st.vault[index].total + 1);
 		}
 	},
 	// Remove item
-	'GESTIOS/ITEMS/DELETE': function (st, { app, id }) {
+	"GESTIOS/ITEMS/DELETE": function (st, { app, id }) {
 		st.vault.forEach((view) => {
 			if (view.app === app) {
-				const index = view.results.findIndex((item) => item._EntityId === id);
+				const index = view.results.findIndex(
+					(item) => item._EntityId === id
+				);
 				if (index > -1) view.results.splice(index, 1);
 			}
 		});
 	},
-	'CLEAR/ITEMS': function (st) {
+	"CLEAR/ITEMS": function (st) {
 		st.vault = [];
-	}
+	},
 };
 
 const actions = {
 	// Wipe view data
-	'gestios/items/wipe': function ({ commit }, view) {
-		commit('GESTIOS/ITEMS/WIPE', view);
+	"gestios/items/wipe": function ({ commit }, view) {
+		commit("GESTIOS/ITEMS/WIPE", view);
 	},
 	// List app items
-	'gestios/items/list': async function ({ commit }, { app, view = null, page = 1, filters = null, order = null, limit = 20, silent = false, reset = false }) {
+	"gestios/items/list": async function (
+		{ commit },
+		{
+			app,
+			view = null,
+			page = 1,
+			filters = null,
+			order = null,
+			limit = 20,
+			silent = false,
+			reset = false,
+		}
+	) {
 		try {
 			if (!silent) utils.loader.start();
-			const result = await gestios.app(app).list({ page, filters, order, limit });
+			const result = await gestios
+				.app(app)
+				.list({ page, filters, order, limit });
 			if (!silent) utils.loader.done();
 			if (!view) view = app;
 
 			if (result.ok) {
-				commit('GESTIOS/ITEMS/LIST', { view, app, data: result.data, total: result.total, reset });
-				commit('GESTIOS/ITEMS/EDIT', { app, data: result.data, ignore: view });
+				commit("GESTIOS/ITEMS/LIST", {
+					view,
+					app,
+					data: result.data,
+					total: result.total,
+					reset,
+				});
+				commit("GESTIOS/ITEMS/EDIT", {
+					app,
+					data: result.data,
+					ignore: view,
+				});
 
 				return result;
 			}
 
 			if (result.code === 404) {
-				commit('GESTIOS/ITEMS/LIST', { view, app, data: [], total: 0, reset });
+				commit("GESTIOS/ITEMS/LIST", {
+					view,
+					app,
+					data: [],
+					total: 0,
+					reset,
+				});
 				return result;
 			}
 
@@ -147,14 +192,17 @@ const actions = {
 		}
 	},
 	// Get app item by ID
-	'gestios/items/get': async function ({ commit }, { app, id, silent = false }) {
+	"gestios/items/get": async function (
+		{ commit },
+		{ app, id, silent = false }
+	) {
 		try {
 			if (!silent) utils.loader.start();
 			const result = await gestios.app(app).get(id);
 			if (!silent) utils.loader.done();
 
 			if (result.ok) {
-				commit('GESTIOS/ITEMS/EDIT', { app, data: [result.data] });
+				commit("GESTIOS/ITEMS/EDIT", { app, data: [result.data] });
 				return result;
 			}
 
@@ -167,7 +215,10 @@ const actions = {
 		}
 	},
 	// Add item
-	'gestios/items/add': async function ({ commit }, { app, params, view = null, callback = null }) {
+	"gestios/items/add": async function (
+		{ commit },
+		{ app, params, view = null, callback = null }
+	) {
 		try {
 			utils.loader.start();
 			const result = await gestios.app(app).add(params, callback);
@@ -176,7 +227,7 @@ const actions = {
 			if (result.ok) {
 				if (!view) view = app;
 
-				commit('GESTIOS/ITEMS/ADD', { view, data: result.data });
+				commit("GESTIOS/ITEMS/ADD", { view, data: result.data });
 				return result;
 			}
 
@@ -189,14 +240,19 @@ const actions = {
 		}
 	},
 	// Edit item
-	'gestios/items/edit': async function ({ commit }, { app, id, params, callback = null }) {
+	"gestios/items/edit": async function (
+		{ commit },
+		{ app, id, params, callback = null }
+	) {
 		try {
 			utils.loader.start();
-			const result = await gestios.app(app).edit({ id, params, callback });
+			const result = await gestios
+				.app(app)
+				.edit({ id, params, callback });
 			utils.loader.done();
 
 			if (result.ok) {
-				commit('GESTIOS/ITEMS/EDIT', { app, data: [result.data] });
+				commit("GESTIOS/ITEMS/EDIT", { app, data: [result.data] });
 				return result;
 			}
 
@@ -209,14 +265,14 @@ const actions = {
 		}
 	},
 	// Toggle item status
-	'gestios/items/status': async function ({ commit }, { app, id }) {
+	"gestios/items/status": async function ({ commit }, { app, id }) {
 		try {
 			utils.loader.start();
 			const result = await gestios.app(app).status(id);
 			utils.loader.done();
 
 			if (result.ok) {
-				commit('GESTIOS/ITEMS/EDIT', { app, data: [result.data] });
+				commit("GESTIOS/ITEMS/EDIT", { app, data: [result.data] });
 				return result;
 			}
 
@@ -224,19 +280,25 @@ const actions = {
 			return result;
 		} catch (err) {
 			utils.loader.done();
-			console.error(`No se pudo cambiar el estado del registro ${id}:${app}`, err);
+			console.error(
+				`No se pudo cambiar el estado del registro ${id}:${app}`,
+				err
+			);
 			return err;
 		}
 	},
 	// Delete app item
-	'gestios/items/delete': async function ({ commit }, { app, id, callback = null }) {
+	"gestios/items/delete": async function (
+		{ commit },
+		{ app, id, callback = null }
+	) {
 		try {
 			utils.loader.start();
 			const result = await gestios.app(app).delete(id, callback);
 			utils.loader.done();
 
 			if (result.ok) {
-				commit('GESTIOS/ITEMS/DELETE', { app, id });
+				commit("GESTIOS/ITEMS/DELETE", { app, id });
 				return result;
 			}
 
@@ -244,15 +306,46 @@ const actions = {
 			return result;
 		} catch (err) {
 			utils.loader.done();
-			console.error('No se pudo obtener las aplicaciones', err);
+			console.error("No se pudo obtener las aplicaciones", err);
 			return err;
 		}
-	}
+	},
+	// List app items
+	"gestios/items/export": async function (
+		{ commit },
+		{
+			app,
+			fields,
+			page = 1,
+			filters = null,
+			order = null,
+			limit = 20,
+			silent = false,
+		}
+	) {
+		try {
+			if (!silent) utils.loader.start();
+			const result = await gestios
+				.app(app)
+				.export({ page, filters, order, limit, fields });
+			if (!silent) utils.loader.done();
+
+			if (result.ok) {
+				return result;
+			}
+
+			if (!silent) utils.error(result);
+			return result;
+		} catch (err) {
+			console.error(`No se pudo export los registros de ${app}`, err);
+			return err;
+		}
+	},
 };
 
 export default {
 	state,
 	getters,
 	actions,
-	mutations
+	mutations,
 };
